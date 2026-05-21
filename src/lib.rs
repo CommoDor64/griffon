3061949eaa0e7ee0ed5bf129d4166280bf8fdbc2
Dor Cohen <commo64dor@pm.me>
2026-05-21 06:30:12 +0200

    changed shr and shl impl to not trait impl due to confusing mut behaviour, a bit of cleanup of functions and corrections of comments

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,16 @@ pub use des::nist_des::canonical_builder;
 pub use des::{BlockState, DES, DESBuilder, DESStage, DESState, FStage, KeyStage, LRKey};
 
 use std::cmp::min;
-use std::ops::{Shl, Shr};
 
 // BitVector allows for arbitrary sized structs of size ceil(N/64).
-// There are two good use cases for it in the scope of griffon - 
-// 1. Arbitrary length keys and states - NIST DES master key is of size 58bit,
-// the round key is 46bit, and state before expansion is 32 , and 48 after.
+// There are two good use cases for it in the scope of griffon -
+// 1. Arbitrary length keys and states - NIST DES master key is of size 56bit,
+// the round key is 48bit, and state before expansion is 32 , and 48 after.
 // This means that we need specialized types to clearly state the intent by a proper
 // type.
 // 2. Allow for larger than 128bit keys, states etc.
 //
-// Internally, it essentially "big-endian", where the first element (element 0) is the first unsigned 64bit segment of 
+// Internally, it essentially "big-endian", where the first element (element 0) is the first unsigned 64bit segment of
 // the vector, and the second is the second. For exmaple:
 //
 // BitVector<128> = [0x01234567_89ABCDEF, 0x11111111_11111111]
@@ -27,7 +26,7 @@ pub(crate) struct BitVector<const N: usize>(Vec<u64>);
 
 impl<const N: usize> BitVector<N> {
     pub(crate) fn new() -> Self {
-        BitVector(Vec::with_capacity((N + 63) / 64))
+        BitVector(Vec::with_capacity(N.div_ceil(64)))
     }
 
     pub(crate) fn add(&mut self, key: &[u64]) {
@@ -35,23 +34,18 @@ impl<const N: usize> BitVector<N> {
             self.0.push(*k);
         }
     }
-}
 
-impl<const N: usize> Shl<usize> for BitVector<N> {
-    type Output = Self;
-
-    fn shl(mut self, rhs: usize) -> Self::Output {
+    pub(crate) fn shl(mut self, rhs: usize) -> Self {
         let mut leftover_bits = rhs % N;
 
         while leftover_bits > 0 {
             let mut prev_leftover = 0u64;
             let shl_bitcount = min(64, leftover_bits);
-            for w in self.0.iter_mut() {
-                let tmp1 = *w;
-                *w <<= shl_bitcount;
+            for w in &mut self.0 {
+                let tmp = *w;
                 prev_leftover >>= 64 - shl_bitcount;
-                *w |= prev_leftover;
-                prev_leftover = tmp1;
+                *w = (*w << shl_bitcount) | prev_leftover;
+                prev_leftover = tmp;
             }
 
             leftover_bits -= shl_bitcount;
@@ -59,23 +53,18 @@ impl<const N: usize> Shl<usize> for BitVector<N> {
 
         self
     }
-}
 
-impl<const N: usize> Shr<usize> for BitVector<N> {
-    type Output = Self;
-
-    fn shr(mut self, rhs: usize) -> Self::Output {
+    pub(crate) fn shr(mut self, rhs: usize) -> Self {
         let mut leftover_bits = rhs % N;
 
         while leftover_bits > 0 {
             let mut prev_leftover = 0u64;
             let shr_bitcount = min(64, leftover_bits);
             for w in self.0.iter_mut().rev() {
-                let tmp1 = *w;
-                *w >>= shr_bitcount;
+                let tmp = *w;
                 prev_leftover <<= 64 - shr_bitcount;
-                *w |= prev_leftover;
-                prev_leftover = tmp1;
+                *w = (*w >> shr_bitcount) | prev_leftover;
+                prev_leftover = tmp;
             }
 
             leftover_bits -= shr_bitcount;
@@ -93,7 +82,7 @@ mod tests {
     fn test_simple_shl() {
         let mut is: BitVector<128> = BitVector::new();
         is.add(&[0x0123_4567_89AB_CDEF, 0x0123_4567_89AB_CDEF]);
-        is = is << 8;
+        is = is.shl(8);
         let should = vec![0x23_4567_89AB_CDEF_00, 0x23_4567_89AB_CDEF_01u64];
         assert_eq!(is.0, should);
     }
@@ -102,7 +91,7 @@ mod tests {
     fn test_simple_shr() {
         let mut is: BitVector<128> = BitVector::new();
         is.add(&[0x0123_4567_89AB_CDEF, 0x0123_4567_89AB_CDEF]);
-        is = is >> 8;
+        is = is.shr(8);
         let should = vec![0xEF_0123_4567_89AB_CD, 0x000123_4567_89AB_CDu64];
         assert_eq!(is.0, should);
     }
@@ -111,7 +100,7 @@ mod tests {
     fn test_multi_round_shl() {
         let mut is: BitVector<128> = BitVector::new();
         is.add(&[0x0123_4567_89AB_CDEF, 0x0123_4567_89AB_CDEF]);
-        is = is << 128 + 8;
+        is = is.shl(128 + 8);
         let should = vec![0x23_4567_89AB_CDEF_00, 0x23_4567_89AB_CDEF_01u64];
         assert_eq!(is.0, should);
     }
@@ -120,7 +109,7 @@ mod tests {
     fn test_multi_round_shr() {
         let mut is: BitVector<128> = BitVector::new();
         is.add(&[0x0123_4567_89AB_CDEF, 0x0123_4567_89AB_CDEF]);
-        is = is >> 128 + 8;
+        is = is.shr(128 + 8);
         let should = vec![0xEF_0123_4567_89AB_CD, 0x000123_4567_89AB_CDu64];
         assert_eq!(is.0, should);
     }
